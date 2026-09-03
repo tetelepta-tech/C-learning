cdaefb7ec7861cd1837a5db6d147c35ee0c340c4
teteleptanuel <nabasaimmanuel@gmail.com>
2026-09-03 14:48:14 +0700

    input username

---
+++ Introduction 1.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+#include <string>
+#include <cctype> //isalnum library (is alphanumeric). Alphanumeric is like A-Z, 0-9, and not a special characther.
+
+using namespace std;
+
+int main (){
+    string name;
+
+    cout << "Hello world\n";
+    cout << "How are u?\n";
+    cout << "What's ur name?\n";
+
+    cin >> name;
+
+    bool isValid = true;
+
+    for(char c : name){
+        if(!isalnum(c)){
+            isValid = false;
+            break;
+        }
+    }
+
+    if(isValid){
+        cout << "Welcome to the groub!\n";
+    } else {
+        cout << "That's the wrong input bro\n";
+    }
+
+    cout << "So, do u want to setup a password?\n";
+
+    string decision;
+
+    cin >> decision;
+    if(decision == "Yes"){
+        cout << "Input ur password";
+    } else if (decision == "No"){
+        cout << "Okay buddy";
+    }
+
+    
+
+
+
+    return 0;
+}
